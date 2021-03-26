813d7f99b5fb603af96d6d9ea6af2efed4279e14
Samuel Gamboa <sd.gamboa.t@gmail.com>
2021-03-26 17:35:25 -0600

    Updated the matrxi_from_dir() function.

--- R/matrix_from_dir.R
+++ R/matrix_from_dir.R
@@ -16,13 +16,14 @@ matrix_from_dir <- function(dir_path) {
     
     list_of_files <- list.files(dir_path)
     names_no_tsv <- gsub(".tsv$", "", list_of_files)
-    list_of_tsvs <- lapply(list_of_files, function(x) readr::read_tsv(paste0(path, "/", x)))
+    list_of_tsvs <- lapply(list_of_files, function(x) readr::read_tsv(paste0(dir_path, "/", x)))
     names(list_of_tsvs) <- names_no_tsv
     df <- purrr::reduce(list_of_tsvs, dplyr::full_join)
     mat <- as.matrix(df[,-1])
     row_names <- df[[1]]
     rownames(mat) <- row_names
     colnames(mat) <- names_no_tsv
+    mat[is.na(mat)] <- 0
     return(mat)
     
 }
